7afe870b719c1c28264d73e02cf1ce11b93685e8
chanchal <chanchal.upadhyay91@gmail.com>
2020-07-27 16:40:36 +0530

    change modulePrefix in config/envirnment

--- .history/config/environment_20200727155448.js
+++ .history/config/environment_20200727155448.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'lift-simulation-emberjs',
+    modulePrefix: 'lift-simulation-ember',
     environment,
     rootURL: '/',
     locationType: 'auto',
